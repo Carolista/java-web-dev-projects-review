0e6e7bda332b789999ffa792b4a537e349961ce0
Carolista <cjones.renaissancewoman@gmail.com>
2023-09-09 23:58:55 -0400

    set up TODOs for chapter 6 studio

--- unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
+++ unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
@@ -5,9 +5,56 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BalancedBracketsTest {
-    //TODO: add tests here
+
+    // POSITIVE TEST CASES
+
+    // TODO: Refactor example test from instructions to use AAA pattern and message parameter
     @Test
-    public void emptyTest() {
-        assertEquals(true, true);
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
     }
+
+    // TODO: Test balanced brackets in front of other characters — "[]LaunchCode"
+
+
+    // TODO: Test balanced brackets around other characters — "[LaunchCode]"
+
+
+    // TODO: Test balanced brackets within other characters — "Launch[Code]"
+
+
+    // TODO: Test multiple consecutive pairs of balanced brackets — "[][][]"
+
+
+    // TODO: Test multiple nested balanced brackets — "[[[]]]"
+
+
+    // NEGATIVE TEST CASES
+
+    // TODO: Test single opening bracket — "["
+
+
+    // TODO: Test single closing bracket — "]"
+
+
+    // TODO: Test reversed brackets — "]["
+
+
+    // TODO: Test single bracket with other characters "[LaunchCode"
+
+
+    // TODO: Test unbalanced brackets among other characters — "Launch]Code["
+
+
+    // TODO: Test a series of mixed, unbalanced brackets — "[[]][]["
+
+
+
+    // EDGE CASES
+
+    // TODO: Test string with only characters other than brackets — "LaunchCode"
+
+
+    // TODO: Test empty string — ""
+
 }
